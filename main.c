0d3704a5209ea0f22025b6288c66c99dc8d19ce3
Md Jahid Hasan <mdjahidhasan150427@gmail.com>
2026-07-25 02:50:56 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int a[5] = {30, 40, 50, 60, 70};
-    int median;
+    float a = 2, b = -5, c = 2;
+    float d, root1, root2;
 
-    median = a[2];
+    d = b * b - 4 * a * c;
 
-    printf("Median = %d", median);
+    if (d > 0)
+    {
+        root1 = (-b + sqrt(d)) / (2 * a);
+        root2 = (-b - sqrt(d)) / (2 * a);
+
+        printf("Root 1 = %.2f\n", root1);
+        printf("Root 2 = %.2f\n", root2);
+    }
+    else if (d == 0)
+    {
+        root1 = -b / (2 * a);
+        printf("Both roots are equal = %.2f\n", root1);
+    }
+    else
+    {
+        printf("The equation has no real roots.\n");
+    }
 
     return 0;
 }
