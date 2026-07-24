8ea73e134cf4add21ced7f4e6b2391133c5832b1
Md Jahid Hasan <mdjahidhasan150427@gmail.com>
2026-07-25 02:29:04 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     int a[5] = {30, 40, 50, 60, 70};
-    int median;
+    int i, sum = 0;
+    float mean, sd, temp = 0;
 
-    median = a[2];
 
-    printf("Median = %d", median);
+    for(i = 0; i < 5; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    mean = (float)sum / 5;
+
+
+    for(i = 0; i < 5; i++)
+    {
+        temp = temp + (a[i] - mean) * (a[i] - mean);
+    }
+
+
+    sd = sqrt(temp / 5);
+
+    printf("Mean = %.2f\n", mean);
+    printf("Standard Deviation = %.2f", sd);
 
     return 0;
 }
